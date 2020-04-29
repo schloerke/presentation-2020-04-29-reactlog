e3a8991ec86be68910ec98fa3d4ef4f6befe479e
Barret Schloerke <schloerke@gmail.com>
2020-04-29 11:31:11 -0400

    add slides about anti-pattern coding

---
+++ scripts/anti-pattern.R
@@ -0,0 +1,30 @@
+library(shiny)
+options("shiny.reactlog" = TRUE)
+
+shinyApp(
+  fluidPage(
+    actionButton("button", "Click me!"),
+    plotOutput("badpattern"),
+    plotOutput("goodpattern"),
+  ),
+  function(input, output) {
+
+    ### BAD
+    observeEvent(input$button, {
+      output$badpattern <- renderPlot({
+        hist(runif(input$button))
+      })
+    })
+
+    ### Good
+    click_count <- reactive({
+      # requires a non-zero (Truthy) value
+      # Prevents the image from appearing until button is clicked
+      req(input$button)
+      input$button
+    })
+    output$goodpattern <- renderPlot({
+      hist(runif(click_count()))
+    })
+
+})
